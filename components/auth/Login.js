5daaaedeb2dfbcc347f9bf88fc9f7af4aa279765
Owen De Waele <owendewa@student.arteveldehs.be>
2019-02-24 14:43:07 +0100

    Added forms

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 
 class Login extends Component{
     state = {
-
+        email = '',
+        password = '',
     }
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit();
+        console.log(this.state);
+    };
     render(){
         return(
-           <h1>Hello there! this is the signin page</h1> 
+            /*
+                Login form
+            * */
+
+           <form>
+               <input placeholder="Email" 
+                value={this.state.email} 
+                onChange={e => this.setState({ email: e.target.value})}>
+                </input>
+                <input placeholder="Password" 
+                value={this.state.password} 
+                onChange={e => this.setState({ password: e.target.value})}>
+                </input>
+                <button onClick={e => this.onSubmit(e)}>Login</button>
+           </form>
+
         )
     }
 }
