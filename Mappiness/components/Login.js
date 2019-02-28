97954346a69ca7f40d67db717a76313128e42227
Owen De Waele <owendewa@student.arteveldehs.be>
2019-02-28 21:58:22 +0100

    Login test

--- Mappiness/components/Login.js
+++ Mappiness/components/Login.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Input } from 'react-native';
 import { LinearGradient } from 'expo';
-import Login from './components/Login'
+import Logo from '../assets/images/logo.png';
 
 export const red = '#DD5630'
 export const yellow = '#FFAB00'
 export const primaryGradientArray = [red, yellow]
 
-export default class App extends React.Component {
+export default class Login extends React.Component {
   render() {
     return (
-      <Login></Login>
+      <LinearGradient colors={ primaryGradientArray } style={styles.container}>
+   
+      </LinearGradient>
     );
   }
 }
