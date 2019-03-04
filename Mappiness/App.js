f6e2fb153c5ef9be10509d04df7201c09bc13124
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-03-04 09:45:20 +0100

    testMarker

--- Mappiness/App.js
+++ Mappiness/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login'
 import Firebase from './base';
 import { MapView, Permissions, Location } from 'expo';
 
+
 export const red = '#DD5630'
 export const yellow = '#FFAB00'
 export const primaryGradientArray = [red, yellow]
@@ -14,6 +15,10 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       region: null,
+      location: {
+        latitude: 53,
+        longitude: 3,
+      }
     }
     this._getLocationAsync();
   }
@@ -26,8 +31,8 @@ export default class App extends React.Component {
     let region = {
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
-      latitudeDelta: 0.045,
-      longitudeDelta: 0.045,
+      latitudeDelta: 0.45,
+      longitudeDelta: 0.45,
     }
     this.setState({region: region})
   }
@@ -37,10 +42,20 @@ export default class App extends React.Component {
       <MapView
         initialRegion={this.state.region}
         showCompass={true}
-        showUserLocation={true}
+        showsUserLocation={false}
         rotateEnabled={false}
         style={{flex: 1}}
-      />
+      >
+        <MapView.Marker
+            coordinate={{
+              latitude: 122.4194,
+              longitude: 37.7749,
+            }}
+        />
+      </MapView>
+
+
+
     );
   }
 }
