d11ef9b5c511f4ec259d926db321da7eb4d17988
Owen De Waele <owendewa@student.arteveldehs.be>
2019-03-10 16:15:28 +0100

    created signupscreen route

--- Mappiness/routes.js
+++ Mappiness/routes.js
@@ -1,6 +1,7 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import LoginScreen from './screens/auth/LoginScreen';
+import SignUpScreen from './screens/auth/SignUpScreen';
 import HomeScreen from './screens/HomeScreen';
 
 const AppNavigator = createStackNavigator({
@@ -9,6 +10,9 @@ const AppNavigator = createStackNavigator({
     },
     Home: {
         screen: HomeScreen,
+    },
+    Signup: {
+        screen: SignUpScreen,
     },
 });
 
