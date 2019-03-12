a29fb0adce1f9c404343de4b5cd3b1de767f4ac7
Owen De Waele <owendewa@student.arteveldehs.be>
2019-03-12 17:59:54 +0100

    Added profile file and route

--- Mappiness/routes.js
+++ Mappiness/routes.js
@@ -3,6 +3,7 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import LoginScreen from './screens/auth/LoginScreen';
 import SignUpScreen from './screens/auth/SignUpScreen';
 import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const AppNavigator = createStackNavigator({
     Login: {
@@ -13,6 +14,9 @@ const AppNavigator = createStackNavigator({
     },
     Signup: {
         screen: SignUpScreen,
+    },
+    Profile:{
+        screen: ProfileScreen,
     },
 });
 
