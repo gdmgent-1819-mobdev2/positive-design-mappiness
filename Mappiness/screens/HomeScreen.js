3d9af6545ea30b9b456449d0bb02b18123aad7ae
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-03-13 15:15:17 +0100

    Get user location and save to database

--- Mappiness/screens/HomeScreen.js
+++ Mappiness/screens/HomeScreen.js
@@ -5,7 +5,7 @@ import { LinearGradient } from 'expo';
 
 import Firebase from '../base';
 import { MapView, Permissions, Location } from 'expo';
-const MarkerImg = require('../assets/images/markertest.jpg');
+const EmojiHappy = require('../assets/icons/emoji/happy.png');
 
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
@@ -41,6 +41,8 @@ export default class HomeScreen extends React.Component {
         latitude: region.latitude,
         longitude: region.longitude,
         }})
+        const uid = firebase.auth().currentUser.uid;
+        firebase.database().ref(`users/${uid}/currentlocation`).set(this.state.loc);
     }
 
     render() {
@@ -49,12 +51,14 @@ export default class HomeScreen extends React.Component {
           initialRegion={this.state.region}
           showCompass={true}
           showsUserLocation={false}
-          rotateEnabled={false}
+          rotateEnabled={true}
           style={{flex: 1}}
         >
           <MapView.Marker
-              //image={MarkerImg}
-              style={styles.mark}
+              image={EmojiHappy}
+              style={{
+                  width: 40
+              }}
               coordinate={this.state.loc}
           />
         </MapView>
@@ -73,7 +77,8 @@ export default class HomeScreen extends React.Component {
       color: '#fff',
     },
     mark: {
-
+        width: 50,
+        display: 'none',
     }
   });
   
