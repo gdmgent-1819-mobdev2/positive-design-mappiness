f759ef7dd8f22ee74d45ce12b2391c1bc263cdc5
Owen De Waele <owendewa@student.arteveldehs.be>
2019-03-15 16:14:45 +0100

    Created provider and consumer file

---
+++ Mappiness/components/userContext.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import * as firebase from 'firebase';
+
+
+const initialState = {
+    personData: {},
+}
+export const UserContext = React.createContext();
+export const AppConsumer = UserContext.Consumer;
+export class AppProvider extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = initialState;
+    }
+  
+    setPersonData = (personData) => {
+      this.setState({personData: personData});
+    }
+  
+    watchPersonData = () => {
+     const userid = firebase.auth().currentUser.uid;
+        firebase.database().ref("users/" + userid).on("value", function(snapshot) {
+        const  personData = snapshot.val();
+        this.setPersonData(personData);
+      }.bind(this), function(error) { });
+    }
+  
+    render() {
+      return (
+        <UserContext.Provider value={{
+          personData: this.state.personData,
+          watchPersonData: this.watchPersonData,
+        }}>
+          {this.props.children}
+        </UserContext.Provider>
+      )
+    }
+  }
