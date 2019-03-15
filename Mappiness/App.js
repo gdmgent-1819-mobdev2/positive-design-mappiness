0c0b2099945f30ab90a3bcc0def78334cfc19329
Owen De Waele <owendewa@student.arteveldehs.be>
2019-03-15 16:13:55 +0100

    Implemented AppProvider on App

--- Mappiness/App.js
+++ Mappiness/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './routes';
+import { AppConsumer, AppProvider } from './components/userContext';
+
 
 export default class App extends React.Component {
 
 
   render() {
     return (
-      <AppNavigator />
+      <AppProvider>
+              <AppNavigator />
+      </AppProvider>
     );
   }
 }
