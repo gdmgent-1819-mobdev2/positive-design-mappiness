a480257f2fd83f1323a56dc4b001ae989f90aa63
Owen De Waele <owendewa@student.arteveldehs.be>
2019-03-15 16:14:58 +0100

    Created dynamic test on profile screen

--- Mappiness/screens/ProfileScreen.js
+++ Mappiness/screens/ProfileScreen.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react';
 import { StyleSheet, View, Text,Switch, TextInput, Button, Alert,TouchableHighlight,TouchableOpacity } from 'react-native';
 import * as firebase from 'firebase';
 import { Avatar } from 'react-native-elements';
+import { AppConsumer, AppProvider } from '../components/userContext';
+
+
 export default class ProfileScreen extends React.Component {
+    componentDidMount() {
+      this.context.watchPersonData(); 
+    }
+
     static navigationOptions = {
         title: 'Profiel',
     };
@@ -16,9 +23,21 @@ export default class ProfileScreen extends React.Component {
       };
     }
     getId = () => {
-      const uid = firebase.auth().currentUser.uid
-      Alert.alert(uid)
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          const uid = firebase.auth().currentUser.uid
+          firebase.database().ref("users/" + uid).on("value", function(snapshot) {
+            const  personData = snapshot.val();
+            console.log(personData);
+          })
+        }
+        else{
+          Alert.alert('Niet ingelogd')
+          this.props.navigation.navigate("Login");
+        }
+      });
     }
+    
     logout = () => {
       firebase.auth().signOut();
       Alert.alert('uitgelogd');
@@ -37,67 +56,73 @@ export default class ProfileScreen extends React.Component {
  
     render() {
       return (
-        <View style={styles.container}>
-          <Avatar
-            rounded
-            source={{uri: "https://pbs.twimg.com/profile_images/814534536830795776/CO84uTK8_400x400.jpg"}}
-            onPress={() => console.log("Works!")}
-            activeOpacity={0.2}
-            size={120}
-          />
-          <Text style={{marginTop: 20,fontWeight:'bold'}}>
-               NAME PERSON
-          </Text>
-          <View style={styles.profileButtons}>
-          <TouchableHighlight  style={styles.button} activeOpacity={1}>
-            <Text style={styles.textButton}>
-                EDIT
-            </Text>
-          </TouchableHighlight>
-          <TouchableHighlight  style={styles.button} onPress={this.logout} activeOpacity={1}>
-            <Text style={styles.textButton}>
-            LOGOUT
-            </Text>
-          </TouchableHighlight>
-          </View>
-          <View>
-            <Button style={styles.button} title="Push me" onPress={this.getId} />
-          </View>
-            <View style={{flexDirection: "row",padding: 2,marginTop: 40}}>
-                <View style={{flex: 1,marginLeft:30}}>
-                    <Text style={styles.profileInfo}>Email</Text>
-                    <Text style={styles.profileInfo}>Nummer</Text>
-                    <Text style={styles.profileInfo}>Adres</Text>
+          <AppConsumer>
+            {(context) => (
+            <View style={styles.container}  ref={(ref) => { this.context = context; }}>
+              <Avatar
+                rounded
+                source={{uri: "https://pbs.twimg.com/profile_images/814534536830795776/CO84uTK8_400x400.jpg"}}
+                onPress={() => console.log("Works!")}
+                activeOpacity={0.2}
+                size={120}
+              />
+              <Text style={{marginTop: 20,fontWeight:'bold'}}>
+                  {context.personData.Naam}
+              </Text>
+              <View style={styles.profileButtons}>
+              <TouchableHighlight  style={styles.button} activeOpacity={1}>
+                <Text style={styles.textButton}>
+                    EDIT
+                </Text>
+              </TouchableHighlight>
+              <TouchableHighlight  style={styles.button} onPress={this.logout} activeOpacity={1}>
+                <Text style={styles.textButton}>
+                LOGOUT
+                </Text>
+              </TouchableHighlight>
+              </View>
+              <View>
+                <Button style={styles.button} title="Push me" onPress={this.getId} />
+              </View>
+                <View style={{flexDirection: "row",padding: 2,marginTop: 40}}>
+                    <View style={{flex: 1,marginLeft:30}}>
+                        <Text style={styles.profileInfo}>Voornaam</Text>
+                        <Text style={styles.profileInfo}>Familienaam</Text>
+                        <Text style={styles.profileInfo}>Email</Text>
+                    </View>
+                    <View style={{flex: 2}}>
+                        <Text style={{marginBottom: 15}}>{context.personData.Naam}</Text>
+                        <Text style={{marginBottom: 15}}>{context.personData.FamilieNaam}</Text>
+                        <Text style={{marginBottom: 15}}>{context.personData.Email}</Text>
+                    </View>
                 </View>
-                <View style={{flex: 2}}>
-                    <Text style={{marginBottom: 15}}>Test</Text>
-                    <Text style={{marginBottom: 15}}>Test</Text>
-                    <Text style={{marginBottom: 15}}>Test</Text>
+                <View style={{flexDirection: "row",padding: 2,marginTop: 40}}>
+                    <View style={{flex: 3,marginLeft:30}}>
+                        <Text style={[styles.profileInfo , styles.border]}>Verder lopen in achtergrond</Text>
+                        <Text style={[styles.profileInfo , styles.border]}>Verberg Naam</Text>
+                        <Text style={[styles.profileInfo , styles.border]}>Update bij gesloten app</Text>
+                    </View>
+                    <View style={{flex: 1}}>
+                      
+                      <Switch onValueChange={isSwitchOn => this.setState({isSwitchOn})} value={this.state.isSwitchOn}/>
+                      <TouchableOpacity style={styles.buttonSwitch} onPress={this.showCalculation}></TouchableOpacity>
+                      
+                      <Switch onValueChange={isSwitchOn2 => this.setState({isSwitchOn2})} value={this.state.isSwitchOn2}/>
+                      <TouchableOpacity style={styles.buttonSwitch} onPress={this.showCalculation}></TouchableOpacity>
+                      
+                      <Switch onValueChange={isSwitchOn3 => this.setState({isSwitchOn3})} value={this.state.isSwitchOn3}/>
+                      <TouchableOpacity style={styles.buttonSwitch} onPress={this.showCalculation}></TouchableOpacity>
+                    
+                      </View>
                 </View>
-            </View>
-            <View style={{flexDirection: "row",padding: 2,marginTop: 40}}>
-                <View style={{flex: 3,marginLeft:30}}>
-                    <Text style={[styles.profileInfo , styles.border]}>Verder lopen in achtergrond</Text>
-                    <Text style={[styles.profileInfo , styles.border]}>Verberg Naam</Text>
-                    <Text style={[styles.profileInfo , styles.border]}>Update bij gesloten app</Text>
+                <View>
+                <Text>dkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkekjbfkezjbfzebfjhezbzekhjbg</Text>
                 </View>
-                <View style={{flex: 1}}>
-                  
-                  <Switch onValueChange={isSwitchOn => this.setState({isSwitchOn})} value={this.state.isSwitchOn}/>
-                  <TouchableOpacity style={styles.buttonSwitch} onPress={this.showCalculation}></TouchableOpacity>
-                  
-                  <Switch onValueChange={isSwitchOn2 => this.setState({isSwitchOn2})} value={this.state.isSwitchOn2}/>
-                  <TouchableOpacity style={styles.buttonSwitch} onPress={this.showCalculation}></TouchableOpacity>
-                  
-                  <Switch onValueChange={isSwitchOn3 => this.setState({isSwitchOn3})} value={this.state.isSwitchOn3}/>
-                  <TouchableOpacity style={styles.buttonSwitch} onPress={this.showCalculation}></TouchableOpacity>
-                
-                  </View>
+              
             </View>
-            <View>
-            <Text>dkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkdkjgkdbgfskbnfskjbnfjksnbmkekjbfkezjbfzebfjhezbzekhjbg</Text>
-            </View>
-        </View>
+            )}
+          </AppConsumer>
+
       );
     }
   }
